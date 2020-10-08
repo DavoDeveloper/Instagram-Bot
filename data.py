05d4a870a8d3507552f5dd7229c757bb013267ac
Davit <davit.poghosyan2.y@tumo.org>
2020-10-08 14:55:24 +0400

    instagram bot

---
+++ data.py
@@ -0,0 +1,17 @@
+username = "nverner_miayn"
+password = "davo123456789"
+
+direct_users_list=['nverner_miayn','davo7.7']
+
+users_settings_dict={
+    'user1':{
+        'login':'nverner_miayn',
+        'password':'davo123456789',
+        'window_size':'800,600'
+    }#,
+    # 'user2':{
+    #     'login':'bot_login',
+    #     'password':'bot_password',
+    #     'window_size':'1600,900'
+    # }
+}
